fcd6e5d706306b637a7f54cb827997e921bc3e7d
klm_grace <66628654+klm-grace@users.noreply.github.com>
2024-04-05 19:14:08 +0100

    Update and clean auth file.

--- auth/src/index.js
+++ auth/src/index.js
@@ -3,21 +3,22 @@ import {logger} from "hono/logger"
 import {cors} from "hono/cors"
 import {routes} from "./routes/index.js";
 import {compress} from 'hono/compress';
-import {handleApiError} from "./common/errro.js";
+import {SERVER_STATUS_CODE} from "../../common/constant.js";
+import {handleApiError} from "../../common/errors.js";
 
 const api = new Hono()
 
 
 api.use("*", compress(), cors(), logger())
 
-api.route("/api/", routes)
+api.route("/", routes)
 
 
 api.onError(handleApiError)
 
 
 api.notFound((c) => {
-    return c.json({message: "The ressources you search do not exists."}, 404)
+    return c.json({message: "The ressources you search do not exists."}, SERVER_STATUS_CODE.NOT_FOUND)
 })
 
 export {
