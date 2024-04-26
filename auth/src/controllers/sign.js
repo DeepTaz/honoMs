066725bd06e74281709e83c79402e89cd9c882fc
Nian <66628654+klm-grace@users.noreply.github.com>
2024-04-26 19:32:44 +0100

    Update.

--- auth/src/controllers/sign.js
+++ auth/src/controllers/sign.js
@@ -1,5 +1,5 @@
 import {createCookie, deleteServerCookie} from "../../../common/honoUtil.js";
-import {COOKIES, SERVER_STATUS_CODE} from "../../../common/constant.js";
+import {COOKIES, STATUS_CODE} from "../../../common/constant.js";
 import {Api_error} from "../../../common/api_error.js";
 import {Users} from "../../../db/schemas/user.js";
 import {sanitizeMongo} from "../../../common/util.js";
@@ -13,7 +13,7 @@ export const signUpController = async function (context) {
     await createCookie({id: user._id}, context)
     return context.json({
         message: "ok"
-    }, SERVER_STATUS_CODE.OK)
+    }, STATUS_CODE.OK)
 
 }
 
@@ -22,21 +22,21 @@ export const signInController = async function (context) {
     const body = sanitizeMongo(await context.req.json())
 
     if (!body.password || !body.phoneNumber) {
-        throw new Api_error("Nous n'acceptons pas de champs vide", "login fail", SERVER_STATUS_CODE.BAD_REQUEST)
+        throw new Api_error("Nous n'acceptons pas de champs vide", "login fail", STATUS_CODE.BAD_REQUEST)
     }
 
     let user = await Users.findOne({phoneNumber: body.phoneNumber}).select("+password")
     let isUser = user && await user.comparePassword(body.password)
 
     if (!user || !isUser) {
-        throw new Api_error('Le mots de passe ou le numéro de telephone est incorrecte😒.', "login fail", SERVER_STATUS_CODE.UNAUTHORIZED)
+        throw new Api_error('Le mots de passe ou le numéro de telephone est incorrecte😒.', "login fail", STATUS_CODE.UNAUTHORIZED)
     }
     // create cookie
     await createCookie({id: user._id}, context)
 
     return context.json({
         message: "ok"
-    }, SERVER_STATUS_CODE.OK)
+    }, STATUS_CODE.OK)
 }
 
 
@@ -44,7 +44,7 @@ export const logoutController = async function (context) {
     deleteServerCookie(context, COOKIES.AUTH)
     return context.json({
         message: "successfully logout👌."
-    }, SERVER_STATUS_CODE.OK)
+    }, STATUS_CODE.OK)
 }
 
 
--- auth/src/controllers/user.js
+++ auth/src/controllers/user.js
@@ -1,4 +1,4 @@
-import {COOKIES, SERVER_STATUS_CODE} from "../../../common/constant.js";
+import {COOKIES, STATUS_CODE} from "../../../common/constant.js";
 import {deleteServerCookie} from "../../../common/honoUtil.js";
 import {Users} from "../../../db/schemas/user.js";
 import {sanitizeMongo} from "../../../common/util.js";
@@ -12,12 +12,12 @@ export const profileController = async function (context) {
         "PUT": async function (context) {
             const body = sanitizeMongo(await context.req.json());
             await Users.findByIdAndUpdate(context.currUser._id, body, {runValidators: true})
-            return context.json(SERVER_STATUS_CODE.NO_CONTENT)
+            return context.json(STATUS_CODE.NO_CONTENT)
         },
         "DELETE": async function (context) {
             await Users.findByIdAndDelete(context.currUser._id)
             deleteServerCookie(context, COOKIES.AUTH_JWT)
-            return context.json(SERVER_STATUS_CODE.NO_CONTENT)
+            return context.json(STATUS_CODE.NO_CONTENT)
         }
     }[context.req.method](context)
 }
